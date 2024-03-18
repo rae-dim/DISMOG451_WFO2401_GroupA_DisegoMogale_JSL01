b1edbafc637a766c971c4de0a13ae28fa184cd9b
Disego <disegorachelm@gmail.com>
2024-03-18 08:40:39 +0200

    seperate js file to check problems

--- practice.js
+++ practice.js
@@ -2,22 +2,27 @@ let input = document.getElementById('petInput').value;
 // Validation logic goes here
 let result = ''; // Placeholder for validation result NOTEW it returns the html object if you use cost/let to declare the variable
 let validPetName = false;
-let validPet = false
-let validYear = false
-let validPetNamed = false
+
+
+
 let current_year = new Date().getFullYear()
 
-function validateSyntax(input) { 
+function valPet(input) { 
+    let validPet = false
     //split the srting input
     const pet = input.innerText.substring(0, 4);
-    const year = input.innerText.substring(4, 8);
-    const petName = input.innerText.substring(8);
+   
 
     if (pet === 'pet_'){
         validPet = true
     }
+    return validPet
+}
 
-    function yearValidation(year) {
+function yearValidation(year) {
+    let validYear = false
+    const year = input.innerText.substring(4, 8);
+
         // Regular expression to match only digits
            let regex =  /^[0-9]+$/;
             if(!RegExp.test(year)) {
@@ -32,23 +37,31 @@ function validateSyntax(input) {
             // Check if year is not in the range 1900 to current year
             if (year < 1900 || year > current_year) {
             return false;
-            }
-            // Year is valid
+            } 
+                //!! How do i bring back tah variable validYyear into the function, not sure where to put it in order to validate it
             return true;
-    }
-    validYear = yearValidation(year)
+            // Year is valid
+            
+}
 
+
+function valPetNamed(input) {
+    let validPetNamed= false
+    const petName = input.innerText.substring(8);
     let petNameRegex = /^([A-Z]{1}[a-z]{2,30})/;
 
     if (petNameRegex.test(petName) ){
     validPetNamed = true;
     }
-return {validPet, validYear, validPetNamed}
+    return validPetNamed
 }
 
-const inputElement = document.getElementById('input').value
-const  {validPet, validYear, validPetNamed} = validSyntax(inputElement)
 
-if (validPetName = validateSyntax(input)){
+const inputElement = document.getElementById('input').value
+validPet= valPet(inputElement)
+validYear= yearValidation(inputElement)
+validPetNamed = validSyntax(inputElement)
+
+if (validPet == false || validYear == false || validPetNamed == false) {
     alert("Wrong Pet Code")
 }
